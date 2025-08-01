0449a9561bbe609f56317b30da23a2d4ab795bca
Juan Tugores <tugorez@gmail.com>
2025-07-31 19:45:01 -0700

    Tweak linear regression to be logistic regression

--- regression/logistic_regression.py
+++ regression/logistic_regression.py
@@ -1,22 +1,24 @@
 import numpy as np
 
-class LinearRegression:
-    def __init__(self, input_dim, output_dim, name = 'LinearRegression'):
+class LogisticRegression:
+    def __init__(self, input_dim, num_classes, name = 'LogisticRegression'):
         self.name = name
         self.input_dim = input_dim
-        self.output_dim = output_dim
+        self.output_dim = num_classes
 
         self.weights = np.random.rand(input_dim, self.output_dim)
         self.bias = np.random.rand(1, self.output_dim)
 
-    def __call__(self, x):
-        return self._predict(x)
+    def __call__(self, x, threshold = 0.5):
+        p = self._predict(x)
+        return (p >= threshold).astype(int)
 
     def __str__(self):
         return f'{self.name}({self.input_dim}, {self.output_dim})'
     
     def _predict(self, x):
-        return x @ self.weights + self.bias
+        linear = x @ self.weights + self.bias
+        return 1 / (1 + np.exp(-linear))
     
     def train(self, x, y, epoch = 1000, learning_rate = 1e-1):
         num_samples = x.shape[0]
@@ -30,7 +32,7 @@ class LinearRegression:
             e = p - y
 
             # Calculate gradients.
-            c = 2 / num_samples
+            c = 1 / num_samples
             grad_w = c * (x.T @ e)
             grad_b = c * np.sum(e, axis = 0, keepdims = True)
 
@@ -38,7 +40,7 @@ class LinearRegression:
             self.weights -= learning_rate * grad_w
             self.bias -= learning_rate * grad_b
             
-            loss = np.sum(e ** 2) / num_samples
+            loss = -np.mean(y * np.log(p +  1e-9) + (1 - y) * np.log(1 - p +  1e-9))
 
             # If it does not improve, halt the training process.
             if (last_loss - loss) == 0:
@@ -52,13 +54,18 @@ class LinearRegression:
                 print(f'({i}/{epoch}) Loss {loss:.8f}')
 
 if __name__ == '__main__':
-    x = np.arange(-1, 1, 0.1).reshape(-1, 1)
-    y = 3 * x + 10
+    x = np.random.randint(0, 256, size=(200, 1))
+    y =  (x >= 128).astype(int)
+    x = x / 255.0
 
-    lr = LinearRegression(input_dim = 1, output_dim = 1)
+    lr = LogisticRegression(input_dim = 1, num_classes = 1)
     lr.train(x, y)
 
-    print(f'm = {lr.weights}, b = {lr.bias}')
+    print("\n--- Making Predictions ---")
+    test_values = np.array([[30], [110], [128], [150], [245]]) / 255.0
+    predictions = lr(test_values)
+    label_map = {0: "Dark ⚫️", 1: "Light ⚪️"}
 
-    results = lr([[30], [40]])
-    print(results)
+    for i, value in enumerate(test_values):
+        prediction_label = label_map[predictions[i][0]]
+        print(f"Grayscale value {value[0] * 255} is classified as: {prediction_label}")
